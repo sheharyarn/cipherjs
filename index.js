eb193b8aaec6e3e8e70f4145887501fe8a226998
Sheharyar Naseer <hello@sheharyar.me>
2017-02-13 03:02:40 -0500

    charToNum and numToChar utility methods

--- index.js
+++ index.js
@@ -1,5 +1,59 @@
 #!/usr/bin/env node
 
-(function(){
+'use strict';
 
-})();
+
+// Utility Methods
+// ---------------
+
+const ASCII_DIFF = 65;
+
+/**
+ * Converts a number in the 0-25 range into
+ * an A-Z character
+ *
+ * @param  {number} num
+ * @return {string}
+ */
+const numToChar = (num) => {
+  return String.fromCharCode(num + ASCII_DIFF);
+};
+
+
+/**
+ * Converts an A-Z character to an integer
+ * in the 0-25 range
+ *
+ * @param  {string} char - One Letter String
+ * @return {number}
+ */
+const charToNum = (char) => {
+  return char.toUpperCase().charCodeAt(0) - ASCII_DIFF;
+}
+
+
+
+// Main Ciphers
+// ------------
+
+const Caesar = {
+  encrypt: (plaintext, key) => {
+  },
+
+  decrypt: (ciphertext, key) => {
+  }
+};
+
+
+
+// Export Modules
+// --------------
+
+module.exports = {
+  Substitution: Caesar,
+  utils: {
+    numToChar: numToChar,
+    charToNum: charToNum
+  }
+};
+
