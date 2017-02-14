ba5bcf497a56491c2aec8145c95bd601a3263cd7
Sheharyar Naseer <hello@sheharyar.me>
2017-02-13 19:00:51 -0500

    Tests for #charToNum

--- test/utils.js
+++ test/utils.js
@@ -13,5 +13,19 @@ describe('utils', () => {
     });
   });
 
+  describe('#charToNum', () => {
+    it('returns numbers in 0-25 range for a-z', () => {
+      utils.charToNum('a').should.equal(0);
+      utils.charToNum('k').should.equal(10);
+      utils.charToNum('z').should.equal(25);
+    });
+
+    it('returns numbers in 0-25 range for A-Z', () => {
+      utils.charToNum('A').should.equal(0);
+      utils.charToNum('K').should.equal(10);
+      utils.charToNum('Z').should.equal(25);
+    });
+  });
+
 });
 
