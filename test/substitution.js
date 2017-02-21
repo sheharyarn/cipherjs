7551668cb214f95231ad0d180fa6ff85461f4df2
Sheharyar Naseer <hello@sheharyar.me>
2017-02-21 16:08:56 -0500

    Wrote Test for Substitution#decrypt

--- test/substitution.js
+++ test/substitution.js
@@ -21,9 +21,32 @@ describe('Simple Substitution Cipher', () => {
         .should.eql('HGIPYWM FWNNHRW PYHP LIGPHUGN NQJWM NWLMWP OHPH')
     });
 
-    it('removes special characters from ciphertexts and keys', () => {
+    it('removes numbers and special characters from ciphertexts and keys', () => {
       Substitution.encrypt('SUBSTITUTION CIPHER', 'Th!5 I5 4 w3!Rd K3y').should.eql('OQHOPAPQPAJG IALYRN')
       Substitution.encrypt('my 53cR3t m3554g3',            'secret key').should.eql('HX CNP HY')
+    });
+  });
+
+
+  describe('#decrypt', () => {
+    it('works for simple keys', () => {
+      Substitution.decrypt('YGOFBQ',        'KEY').should.eql('CIPHER')
+      Substitution.decrypt('RZJKKKKPLK', 'ZEBRAS').should.eql('DAMNNNNSON')
+    });
+
+    it('retains ciphertext spaces in plaintext', () => {
+      Substitution.decrypt('JY QTMSP QSXPSR JSQQFBS', 'FOXES')
+        .should.eql('MY SUPER SECRET MESSAGE')
+    });
+
+    it('works for keys with repeating characters', () => {
+      Substitution.decrypt('HGIPYWM FWNNHRW PYHP LIGPHUGN NQJWM NWLMWP OHPH', 'HELLOHOWAREYOU')
+        .should.eql('ANOTHER MESSAGE THAT CONTAINS SUPER SECRET DATA')
+    });
+
+    it('removes numbers and special characters from ciphertexts and keys', () => {
+      Substitution.decrypt('OQHOPAPQPAJG IALYRN', 'Th!5 I5 4 w3!Rd K3y').should.eql('SUBSTITUTION CIPHER')
+      Substitution.decrypt('HX 53CN3P H3554Y3',            'secret key').should.eql('MY CRT MG')
     });
   });
 
