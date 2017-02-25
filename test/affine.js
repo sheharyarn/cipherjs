6ca12a5de0eb92e266d2c9adf2423e5e8f9698c3
Sheharyar Naseer <hello@sheharyar.me>
2017-02-25 16:11:06 -0500

    Tests for Affine#decrypt

--- test/affine.js
+++ test/affine.js
@@ -31,5 +31,30 @@ describe('Affine Cipher', () => {
     });
   });
 
+  describe('#decrypt', () => {
+    it('throws error if "a" key is not coprime with 26', () => {
+      ( () => Affine.decrypt('HELLO', 2, 10) ).should.throw(RangeError);
+      ( () => Affine.decrypt('HELLO', 8, 23) ).should.throw(RangeError);
+      ( () => Affine.decrypt('HELLO', 13, 5) ).should.throw(RangeError);
+    });
+
+    it('throws error if "b" key is not between 0-25', () => {
+      ( () => Affine.decrypt('HELLO', 1,  30) ).should.throw(RangeError);
+      ( () => Affine.decrypt('HELLO', 11, 55) ).should.throw(RangeError);
+    });
+
+    it('correctly decrypts ciphertexts for valid keys', () => {
+      Affine.decrypt('MJQQT',          1,  5).should.eql('HELLO');
+      Affine.decrypt('JHDUHYXHJJZLH', 15, 25).should.eql('SECRETMESSAGE');
+    });
+
+    it('retains plaintext\'s spaces', () => {
+      Affine.decrypt('K N Q T W Z', 3, 10).should.eql('A B C D E F');
+    });
+
+    it('removes all numbers and special characters', () => {
+      Affine.decrypt('R%9 Q0 @U W!F#Z C?I', 19, 20).should.eql('T I A WRD KY');
+    });
+  });
 });
 
