227977c3fdbd5779844c8a3b16c039e003361e26
Sheharyar Naseer <hello@sheharyar.me>
2017-02-28 18:02:15 -0500

    Implemented Vigenere#decrypt

--- index.js
+++ index.js
@@ -184,12 +184,35 @@ const Vigenere = {
       .replace(/\ +/g, ' ')
       .split(' ')
       .map((s)  => utils.stringToNumArray(s))
-      .map((na) => na.map((n) => keyIndices[counter++ % length] + n))
+      .map((na) => na.map((n) => n + keyIndices[counter++ % length]))
+      .map((na) => na.map((n) => utils.bringInRange(n)))
+      .map((na) => utils.numArrayToString(na))
+      .join(' ');
+  },
+
+
+  /**
+   * Decrypts a Ciphertext with the given string key
+   * using Vigenere Cipher
+   *
+   * @param  {string} ciphertext
+   * @param  {string} key
+   * @return {string} plaintext
+   */
+  decrypt: (plaintext, key) => {
+    const keyIndices = utils.stringToNumArray(key);
+    const length     = keyIndices.length;
+    let   counter    = 0;
+
+    return plaintext
+      .replace(/\ +/g, ' ')
+      .split(' ')
+      .map((s)  => utils.stringToNumArray(s))
+      .map((na) => na.map((n) => n - keyIndices[counter++ % length]))
       .map((na) => na.map((n) => utils.bringInRange(n)))
       .map((na) => utils.numArrayToString(na))
       .join(' ');
   }
-
 };
 
 
