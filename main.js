5f3d29e414b1bc146d0b3d5eaa96a1b08849dfaa
Sheharyar Naseer <hello@sheharyar.me>
2017-03-01 16:27:06 -0500

    Using inquirer for getting user's inputs

--- main.js
+++ main.js
@@ -19,6 +19,44 @@
   ];
 
 
+  // Quit Message for the User
+  const quitChoices = [new inquirer.Separator(), { name: 'Quit', value: -1 }];
+
+
+  // Cipher Choices to show to the user
+  const cipherChoices =
+    ciphers
+      .map((t,i) => ({ value: i, name: t.name }))
+      .concat(quitChoices);
+
+
+
+  // Check if the user wants to quit or not
+  const validateAnswer = ({ input }) => {
+    if (input === -1) {
+      console.log('\nBye!\n');
+      process.exit();
+    } else {
+      return input;
+    }
+  }
+
+
+  // Ask the User which Cipher he wants to use
+  const getCipherInput = () => {
+    inquirer
+      .prompt({
+        name:    'input',
+        type:    'list',
+        message: 'Choose an Option',
+        choices: cipherChoices
+      })
+      .then(validateAnswer)
+      .then(() => {});
+  }
+
+
+
 
 
 console.log(`
@@ -29,6 +67,7 @@ Cipher Utility
 
 `)
 
+getCipherInput();
 
 
 })();
